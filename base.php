c8fea3dcb21bd3a70dcd8e11834c4cfbd31f7845
QCman <rkomron88@gmail.com>
2024-06-10 11:14:22 +0500

    Update base.php

--- base.php
+++ base.php
@@ -1,14 +1,17 @@
 <?php
 
+// Подключаемся к базе данных
 $mysqli = new mysqli("localhost", "root", "password", "watcoin");
 
+// Получаем текущие значения счетчика и лимита из локального хранилища или устанавливаем их в 0
 $count = +$_POST["count"] || 0;
+$limit = +$_POST["limit"] || 2000;
 
-$stmt = $mysqli->prepare("INSERT INTO results (count) VALUES (?)");
-$stmt->bind_param("i", $count);
+// Сохраняем счетчик и лимит в базе данных
+$stmt = $mysqli->prepare("INSERT INTO results (count, limit) VALUES (?, ?)");
+$stmt->bind_param("ii", $count, $limit);
 $stmt->execute();
 
+// Закрываем соединение с базой данных
 $stmt->close();
-$mysqli->close();
-
-?>
+$mysqli->close
